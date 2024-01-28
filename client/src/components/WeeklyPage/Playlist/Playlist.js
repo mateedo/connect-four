7aeb58e5ffc4bcffd27cc841d3b47b15fb1e1c7f
David Hu <badjokesdavid@gmail.com>
2024-01-28 14:08:45 -0500

    weekly page setup + server.py fix

--- client/src/components/WeeklyPage/Playlist/Playlist.js
+++ client/src/components/WeeklyPage/Playlist/Playlist.js
@@ -4,7 +4,7 @@ import "./Playlist.css";
 export default function Playlist(props) {
     return(
         <div className="playlist">
-            
+      
             <Track />
         </div>
     )
--- client/src/components/WeeklyPage/Playlist/Track/Track.js
+++ client/src/components/WeeklyPage/Playlist/Track/Track.js
@@ -1,9 +1,15 @@
 import "./Track.css";
-
+import CoverArt from "../../../../images/spotifytest.jpg";
 export default function Track(props) {
     return(
         <div className="track">
-        
+            <div className="track-info">
+                <img src={CoverArt} alt="Cover Art" />
+                <div className="track-info-text">
+                    <h3>Track Name</h3>
+                    <h4>Artist Name</h4>
+                </div>
+            </div>
         </div>
     )
 }
