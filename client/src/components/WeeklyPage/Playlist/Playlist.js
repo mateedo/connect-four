c2605885a008ea82bf5214b56d2932d8c0ae6b1c
David Hu <badjokesdavid@gmail.com>
2024-04-26 03:50:43 -0400

    set up playlist and track components

--- client/src/components/WeeklyPage/Playlist/Playlist.js
+++ client/src/components/WeeklyPage/Playlist/Playlist.js
@@ -1,11 +1,58 @@
 import Track from "./Track/Track";
 import "./Playlist.css";
-
+import CoverArt from "../../../images/spotifytest.jpg";
 export default function Playlist(props) {
     return(
         <div className="playlist">
       
-            <Track />
+            <Track 
+                trackNum="1"
+                artistName="Eminem"
+                coverArt={CoverArt}
+                trackName="Godzilla"
+                albumName="Music To Be Murdered By"
+                duration="3:31"
+            />
+            <Track
+                trackNum="2"
+                artistName="The Weeknd"
+                coverArt={CoverArt}
+                trackName="Blinding Lights"
+                albumName="After Hours"
+                duration="3:20"
+            />
+            <Track
+                trackNum="3"
+                artistName="Three Days Grace"
+                coverArt={CoverArt}
+                trackName="Never Too Late"
+                albumName="One-X"
+                duration="3:29"
+            />
+            <Track 
+                trackNum="4"
+                artistName="Eminem"
+                coverArt={CoverArt}
+                trackName="Godzilla"
+                albumName="Music To Be Murdered By"
+                duration="3:31"
+            />
+            <Track
+                trackNum="5"
+                artistName="The Weeknd"
+                coverArt={CoverArt}
+                trackName="Blinding Lights"
+                albumName="After Hours"
+                duration="3:20"
+            />
+            <Track
+                trackNum="6"
+                artistName="Three Days Grace"
+                coverArt={CoverArt}
+                trackName="Never Too Late"
+                albumName="One-X"
+                duration="3:29"
+            />
         </div>
     )
 }
