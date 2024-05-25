bd1bde21c038b4b8573130625325798e3e09050c
David Hu <badjokesdavid@gmail.com>
2024-05-25 03:25:28 -0400

    Changed track info to map from array

--- client/src/components/WeeklyPage/Playlist/Playlist.js
+++ client/src/components/WeeklyPage/Playlist/Playlist.js
@@ -2,10 +2,48 @@ import Track from "./Track/Track";
 import "./Playlist.css";
 import CoverArt from "../../../images/spotifytest.jpg";
 export default function Playlist(props) {
+    const trackInfo = [ {
+        trackNum: "1",
+        artistName: "Eminem",
+        trackName: "Godzilla",
+        albumName: "Music To Be Murdered By",
+        duration: "3:31",
+    },
+    {
+        trackNum: "2",
+        artistName: "The Weeknd",
+        trackName: "Blinding Lights",
+        albumName: "After Hours",
+        duration: "3:20",
+    },
+    {
+        trackNum: "3",
+        artistName: "Three Days Grace",
+        trackName: "Never Too Late",
+        albumName: "One-X",
+        duration: "3:29",
+    },
+    {
+        trackNum: "4",
+        artistName: "Eminem",
+        trackName: "Godzilla",
+        albumName: "Music To Be Murdered By"
+    }]
     return(
         <div className="playlist">
-      
-            <Track 
+            {trackInfo.map((track, index) => {
+                return <Track 
+                    key={index}
+                    trackNum={track.trackNum}
+                    artistName={track.artistName}
+                    coverArt={CoverArt}
+                    trackName={track.trackName}
+                    albumName={track.albumName}
+                    duration={track.duration}
+                />
+            })
+            }
+            {/* <Track 
                 trackNum="1"
                 artistName="Eminem"
                 coverArt={CoverArt}
@@ -52,7 +90,7 @@ export default function Playlist(props) {
                 trackName="Never Too Late"
                 albumName="One-X"
                 duration="3:29"
-            />
+            /> */}
         </div>
     )
 }
